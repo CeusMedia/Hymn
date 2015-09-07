8ed7334bfc93f7cb404071ca728aa675897c76c6
kriss0r <kriss@ceusmedia.de>
2015-09-08 01:45:57 +0200

    Implement new command: version.

---
+++ src/classes/Command/Version.php
@@ -0,0 +1,7 @@
+<?php
+class Hymn_Command_Version extends Hymn_Command_Abstract implements Hymn_Command_Interface{
+
+	public function run( $arguments = array() ){
+		Hymn_Client::out( Hymn_Client::$version );
+	}
+}
