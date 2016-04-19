4c7b8cb2ce841ee210e1c796dc02cd1cabb5bb34
Christian Würker <kriss@ceusmedia.de>
2016-04-19 02:18:26 +0200

    Extend SQL by module update and dry mode.

--- src/classes/Module/SQL.php
+++ src/classes/Module/SQL.php
@@ -11,8 +11,30 @@ class Hymn_Module_SQL{
 		$this->quiet	= $quiet;
 	}
 
+	/**
+	 *	Checks for database connection and returns used PDO driver.
+	 *	@access		protected
+	 *	@return		string							PDO driver used by database connection
+	 *	@throws		RuntimeException				if no database connection is available
+	 */
+	protected function checkDriver(){
+		if( !$this->client->getDatabase() )															//  database connection is not established yet
+			$this->client->setupDatabaseConnection( TRUE );											//  setup database connection
+		$driver	= $this->client->getDatabaseConfiguration( 'driver' );								//  get database driver
+		if( !$driver )																				//  no database driver set
+			throw new RuntimeException( 'No database connection available' );
+		return $driver;
+	}
+
+	/**
+	 *	Executes single SQL script against database connection.
+	 *	@access		protected
+	 *	@param 		string 		$sql				SQL script to execute
+	 *	@return		void
+	 *	@throws		RuntimeException				if execution fails
+	 */
 	protected function executeSql( $sql ){
-		$dbc		= $this->client->setupDatabaseConnection();
+//		$dbc		= $this->client->setupDatabaseConnection();
 		$dbc		= $this->client->getDatabase();
 		$prefix		= $this->client->getDatabaseConfiguration( 'prefix' );
 		$lines		= explode( "\n", trim( $sql ) );
@@ -47,113 +69,141 @@ class Hymn_Module_SQL{
 	/**
 	 *	Reads module SQL scripts and executes install and update scripts.
 	 *	@access		public
-	 *	@param 		object 		$module		Module object
-	 *	@param 		boolean 	$verbose	Flag: be verbose
+	 *	@param 		object 		$module				Object of nodule to install
+	 *	@param 		boolean 	$verbose			Flag: be verbose
+	 *	@param		boolean		$dry				Flag: dry run mode - simulation only
 	 *	@return		void
 	 *	@throws		RuntimeException		if target file is not readable
 	 */
-	public function runModuleInstallSql( $module, $verbose ){
-		if( isset( $module->sql ) && count( $module->sql ) ){										//  module has SQL scripts
-			if( !$this->client->getDatabase() )														//  database connection is not established yet
-				$this->client->setupDatabaseConnection( TRUE );										//  setup database connection
-			$driver	= $this->client->getDatabaseConfiguration( 'driver' );							//  get database driver
-			if( !$driver ){																			//  no database driver set
-				$msg	= 'Cannot install SQL of module "%s": No database connection available';
-				throw new RuntimeException( sprintf( $msg, $module->id ) );
-			}
-			$version	= 0;																		//  init reached version
-			$scripts	= array();																	//  prepare empty list for collected scripts
+	public function runModuleInstallSql( $module, $verbose, $dry = FALSE ){
+		if( !isset( $module->sql ) || !count( $module->sql ) )										//  module has no SQL scripts
+			return;																					//  quit here
+		$driver		= $this->checkDriver();															//  check database connection and get PDO driver
+		$version	= 0;																			//  init reached version
+		$scripts	= array();																		//  prepare empty list for collected scripts
 
-			foreach( $module->sql as $sql )															//  first run: install
-				if( $sql->event == "install" && trim( $sql->sql ) )									//  is an install script
-					if( $sql->version == "final" || !$sql->version )								//  is final install script
-						if( $sql->type === $driver || $sql->type == "*" )							//  database driver is matching or general
+		foreach( $module->sql as $sql )																//  first run: install
+			if( $sql->event == "install" && trim( $sql->sql ) )										//  is an install script
+				if( $sql->version == "final" || !$sql->version )									//  is final install script
+					if( $sql->type === $driver || $sql->type == "*" )								//  database driver is matching or general
+						$scripts[]	= $sql;															//  append script for execution
+
+		if( !$scripts ){
+			foreach( $module->sql as $sql ){														//  first run: install
+				if( version_compare( $version, $module->version, "<" ) ){							//  reached version is not final
+					if( $sql->type === $driver || $sql->type == "*" ){								//  database driver is matching or general
+						if( $sql->event == "install" && trim( $sql->sql ) ){						//  is an install script
+							if( isset( $sql->version ) )											//  script version is set
+								$version	= $sql->version;										//  set reached version to script version
 							$scripts[]	= $sql;														//  append script for execution
-
-			if( !$scripts ){
-				foreach( $module->sql as $sql ){													//  first run: install
-					if( version_compare( $version, $module->version, "<" ) ){						//  reached version is not final
-						if( $sql->type === $driver || $sql->type == "*" ){							//  database driver is matching or general
-							if( $sql->event == "install" && trim( $sql->sql ) ){					//  is an install script
-								if( isset( $sql->version ) )										//  script version is set
-									$version	= $sql->version;									//  set reached version to script version
-								$scripts[]	= $sql;													//  append script for execution
-							}
 						}
 					}
 				}
-				foreach( $module->sql as $sql ){													//  second run: update
-					if( version_compare( $version, $module->version, "<" ) ){						//  reached version is not final
-						if( $sql->type === $driver || $sql->type == "*" ){							//  database driver is matching or general
-							if( $sql->event == "update" && trim( $sql->sql ) ){						//  is an update script
-								if( isset( $sql->version ) ){										//  script version is set
-									if( version_compare( $version, $sql->version, "<" ) ){			//  script version is greater than reached version
-										$version	= $sql->version;								//  set reached version to script version
-										$scripts[]	= $sql;											//  append script for execution
-									}
+			}
+			foreach( $module->sql as $sql ){														//  second run: update
+				if( version_compare( $version, $module->version, "<" ) ){							//  reached version is not final
+					if( $sql->type === $driver || $sql->type == "*" ){								//  database driver is matching or general
+						if( $sql->event == "update" && trim( $sql->sql ) ){							//  is an update script
+							if( isset( $sql->version ) ){											//  script version is set
+								if( version_compare( $version, $sql->version, "<" ) ){				//  script version is greater than reached version
+									$version	= $sql->version;									//  set reached version to script version
+									$scripts[]	= $sql;												//  append script for execution
 								}
 							}
 						}
 					}
 				}
 			}
-			foreach( $scripts as $script ){															//  iterate collected scripts
-				if( $verbose && !$this->quiet ){													//  be verbose
-					$msg	= "    … apply database script on %s at version %s";
-					Hymn_Client::out( sprintf( $msg, $script->event, $script->version ) );
-				}
-				$this->executeSql( $script->sql );													//  execute collected SQL script
+		}
+		foreach( $scripts as $script ){																//  iterate collected scripts
+			if( $verbose && !$this->quiet ){														//  be verbose
+				$msg	= "    … apply database script on %s at version %s";
+				Hymn_Client::out( sprintf( $msg, $script->event, $script->version ) );
 			}
+			if( !$dry )																				//  no a dry run
+				$this->executeSql( $script->sql );													//  execute collected SQL script
 		}
 	}
 
 	/**
 	 *	Reads module SQL scripts and executes install and update scripts.
 	 *	@access		public
-	 *	@param 		object 		$module		Module object
-	 *	@param 		boolean 	$verbose	Flag: be verbose
+	 *	@param 		object 		$installedModule	Object of locally installed module
+	 *	@param 		boolean 	$verbose			Flag: be verbose
+	 *	@param		boolean		$dry				Flag: dry run mode - simulation only
 	 *	@return		void
 	 */
-	public function runModuleUninstallSql( $module, $verbose ){
-		if( isset( $module->sql ) && count( $module->sql ) ){										//  module has SQL scripts
-			if( !$this->client->getDatabase() )														//  database connection is not established yet
-				$this->client->setupDatabaseConnection( TRUE );										//  setup database connection
-			$driver	= $this->client->getDatabaseConfiguration( 'driver' );							//  get database driver
-			if( !$driver ){																			//  no database driver set
-				$msg	= 'Cannot install SQL of module "%s": No database connection available';
-				throw new RuntimeException( sprintf( $msg, $module->id ) );
-			}
+	public function runModuleUninstallSql( $installedModule, $verbose, $dry = FALSE ){
+		if( !isset( $installedModule->sql ) || !count( $installedModule->sql ) )					//  module has no SQL scripts
+			return;																					//  quit here
+		$driver		= $this->checkDriver();															//  check database connection and get PDO driver
+		$version	= 0;																			//  init reached version
+		$scripts	= array();																		//  prepare empty list for collected scripts
 
-			$version	= 0;																		//  init reached version
-			$scripts	= array();																	//  prepare empty list for collected scripts
+		foreach( $installedModule->sql as $sql )													//  first run: uninstall
+			if( $sql->event == "uninstall" && trim( $sql->sql ) )									//  is an uninstall script
+				if( $sql->version == "final" )														//  is final uninstall script
+					if( $sql->type === $driver || $sql->type == "*" )								//  database driver is matching or general
+						$scripts[]	= $sql;															//  append script for execution
 
-			foreach( $module->sql as $sql )															//  first run: uninstall
-				if( $sql->event == "uninstall" && trim( $sql->sql ) )								//  is an uninstall script
-					if( $sql->version == "final" )													//  is final uninstall script
-						if( $sql->type === $driver || $sql->type == "*" )							//  database driver is matching or general
+		if( !$scripts ){
+			foreach( $installedModule->sql as $sql ){												//  iterate SQL scripts
+				if( version_compare( $version, $installedModule->version, "<" ) ){					//  reached version is not final
+					if( $sql->type === $driver || $sql->type == "*" ){								//  database driver is matching or general
+						if( $sql->event == "uninstall" && trim( $sql->sql ) ){						//  is an uninstall script
 							$scripts[]	= $sql;														//  append script for execution
-
-			if( !$scripts ){
-				foreach( $module->sql as $sql ){													//  iterate SQL scripts
-					if( version_compare( $version, $module->version, "<" ) ){						//  reached version is not final
-						if( $sql->type === $driver || $sql->type == "*" ){							//  database driver is matching or general
-							if( $sql->event == "uninstall" && trim( $sql->sql ) ){					//  is an uninstall script
-								$scripts[]	= $sql;													//  append script for execution
-								if( $sql->version == "final" )										//  is final uninstall script
-									$version	= $module->version;									//  set version to module version to skip others
-							}
+							if( $sql->version == "final" )											//  is final uninstall script
+								$version	= $installedModule->version;							//  set version to module version to skip others
 						}
 					}
 				}
 			}
+		}
 
-			foreach( $scripts as $script ){
-				if( $verbose && !$this->quiet ){													//  be verbose
-					$msg	= "    … apply database script on %s at version %s";
-					Hymn_Client::out( sprintf( $msg, $script->event, $script->version ) );
-				}
-				$this->executeSql( $script->sql );													//  execute collected SQL script
+		foreach( $scripts as $script ){
+			if( $verbose && !$this->quiet ){														//  be verbose
+				$msg	= "    … apply database script on %s at version %s";
+				Hymn_Client::out( sprintf( $msg, $script->event, $script->version ) );
 			}
+			if( !$dry )																				//  not a dry run
+				$this->executeSql( $script->sql );													//  execute collected SQL script
+		}
+	}
+
+	/**
+	 *	Reads module SQL scripts and executes install and update scripts.
+	 *	@access		public
+	 *	@param 		object 		$installedModule	Object of locally installed module
+	 *	@param 		object 		$module				Object of library module to update to
+	 *	@param 		boolean 	$verbose			Flag: be verbose
+	 *	@param		boolean		$dry				Flag: dry run mode - simulation only
+	 *	@return		void
+	 */
+	public function runModuleUpdateSql( $installedModule, $module, $verbose, $dry = FALSE ){
+		if( !isset( $module->sql ) || !count( $module->sql ) )										//  module has no SQL scripts
+			return;																					//  quit here
+		$driver		= $this->checkDriver();															//  check database connection and get PDO driver
+		$version	= $installedModule->version;													//  start by version of currently installed module
+		$scripts	= array();																		//  prepare empty list for collected scripts
+
+		foreach( $module->sql as $sql ){															//  iterate SQL scripts
+			if( $sql->event == "update" && trim( $sql->sql ) ){										//  is an update script
+				if( $sql->type === $driver || $sql->type == "*" ){									//  database driver is matching or general
+					if( version_compare( $sql->version, $version,">" ) ){							//  script version is prior to currently installed version
+						$scripts[$sql->version]	= $sql;												//  append script for execution
+					}
+				}
+			}
+		}
+		uksort( $scripts, 'version_compare' );														//  sort update scripts by version
+
+		foreach( $scripts as $script ){																//  iterate found ordered update scripts
+			if( $verbose && !$this->quiet ){														//  be verbose
+				$msg	= "    … apply database script on %s at version %s";						//  ...
+				Hymn_Client::out( sprintf( $msg, $script->event, $script->version ) );				//  ...
+			}
+			if( !$dry )																				//  not a dry run
+				$this->executeSql( $script->sql );													//  execute collected SQL script
 		}
 	}
 }
