ff5f09c157e3c9d4eb2903c96015307a7a021051
kriss0r <kriss@ceusmedia.de>
2016-04-29 14:30:08 +0200

    Improve class syntax testing.

--- src/classes/Command/Test.php
+++ src/classes/Command/Test.php
@@ -2,10 +2,23 @@
 class Hymn_Command_Test extends Hymn_Command_Abstract implements Hymn_Command_Interface{
 
 	public function run(){
-		Hymn_Test::checkPhpClasses( "./", FALSE, !FALSE );
+		$this->client->arguments->registerOption( 'recursive', '/^-r|--recursive$/', TRUE );
+		$this->client->arguments->parse();
+
+		$this->recursive	= $this->client->arguments->getOption( 'recursive' );
+		$this->quiet		= $this->client->arguments->getOption( 'quiet' );
+		$this->verbose		= $this->client->arguments->getOption( 'verbose' );
+
+		$path	= $this->client->arguments->getArgument( 0 );
+		if( !$path )
+			$path	= ".";
+
+		Hymn_Test::checkPhpClasses( $path, $this->recursive, $this->verbose );
+
+/*		Hymn_Test::checkPhpClasses( "./", FALSE, !FALSE );
 		if( file_exists( "classes" ) )
 			Hymn_Test::checkPhpClasses( "./classes", TRUE, !FALSE );
 		if( file_exists( "templates" ) )
 			Hymn_Test::checkPhpClasses( "./templates", TRUE, !FALSE );
-	}
+*/	}
 }
