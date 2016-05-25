3a1cbbee6e1f9b4eecd877e98aaf856d1877421f
Christian Würker <kriss@ceusmedia.de>
2016-05-25 04:53:28 +0200

    Strip and compress PHAR files duration build.

--- build/create.php
+++ build/create.php
@@ -4,7 +4,6 @@ $pharFileName	= 'hymn.phar';
 $pharFilePath	= $rootPath.'/'.$pharFileName;
 $mainFileName	= 'hymn.php';
 
-
 $archive		= new Phar(
 	$pharFilePath,
 	FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
@@ -24,6 +23,16 @@ __HALT_COMPILER(); ?>' );
 $archive->addFromString( $mainFileName, file_get_contents( __DIR__.'/'.$mainFileName ) );
 $archive->addFile( $rootPath.'/src/locales/en/help/default.txt', 'locales/en/help/default.txt' );
 $archive->addFile( $rootPath.'/src/locales/en/help/reflect-options.txt', 'locales/en/help/reflect-options.txt' );
-$archive->buildFromDirectory( $rootPath.'/src/classes/', '$(.*)\.php$' );
+
+shell_exec( "cp -r ".$rootPath."/src/classes ".$rootPath."/build/" );
+$directory	= new RecursiveDirectoryIterator( $rootPath."/build/classes", RecursiveDirectoryIterator::SKIP_DOTS );
+$iterator	= new RecursiveIteratorIterator( $directory, RecursiveIteratorIterator::CHILD_FIRST );
+foreach( $iterator as $entry )
+	if( !$entry->isDir() )
+		file_put_contents( $entry->getPathname(), php_strip_whitespace( $entry->getPathname() ) );
+
+$archive->buildFromDirectory( $rootPath.'/build/classes/', '$(.*)\.php$' );
+$archive->compressFiles( Phar::GZ );
 $archive->stopBuffering();
+shell_exec( "rm -rf ".$rootPath."/build/classes" );
 ?>
