0603d98de62589e4af9f4e62df6041e48f6c5180
Christian Würker <kriss@ceusmedia.de>
2020-06-14 03:21:03 +0200

    Add new command app-hooks.

---
+++ src/classes/Command/App/Hooks.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ *	...
+ *
+ *	Copyright (c) 2014-2020 Christian Würker (ceusmedia.de)
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	@category		Tool
+ *	@package		CeusMedia.Hymn.Command
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2014-2020 Christian Würker
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/Hymn
+ */
+/**
+ *	...
+ *
+ *	@category		Tool
+ *	@package		CeusMedia.Hymn.Command
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2014-2020 Christian Würker
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/Hymn
+ *	@todo    		code documentation
+ */
+class Hymn_Command_App_Hooks extends Hymn_Command_Abstract implements Hymn_Command_Interface{
+
+	/**
+	 *	Execute this command.
+	 *	Implements flags:
+	 *	Missing flags: verbose
+	 *	@todo		implement missing flags
+	 *	@access		public
+	 *	@return		void
+	 */
+	public function run(){
+		if( !file_exists( Hymn_Client::$fileName ) )
+			throw new RuntimeException( "Hymn project '".Hymn_Client::$fileName."' is missing. Please run 'hymn init'!" );
+
+		$list			= array();
+		$library		= $this->getLibrary();
+		$inlineFuncs	= 0;
+		foreach( $library->listInstalledModules() as $moduleId => $module ){
+			foreach( $module->hooks as $resource => $events ){
+				foreach( $events as $event => $functions ){
+					foreach( $functions as $function ){
+						if( !preg_match( '/\n/', $function ) ){
+							$id	= $resource.'_'.$event.'_'.$moduleId.'_'.$function;
+							$list[$id]	= (object) array(
+								'moduleId'		=> $moduleId,
+								'resource'		=> $resource,
+								'event'			=> $event,
+								'type'			=> 'staticPublicFunctionCall',
+								'function'		=> $function,
+							);
+						}
+						else{
+							$inlineFuncs++;
+							$id	= vsprintf( '%s_%s_%s_func-%s', array(
+								$resource,
+								$event,
+								$moduleId,
+								str_pad( $inlineFuncs, 4, '0', STR_PAD_LEFT ),
+							) );
+							$list[$id]	= (object) array(
+								'moduleId'		=> $moduleId,
+								'resource'		=> $resource,
+								'event'			=> $event,
+								'type'			=> 'inlineFunction',
+								'function'		=> '<inline_function>',
+							);
+						}
+					}
+				}
+			}
+		}
+		ksort( $list );
+
+		foreach( $list as $hook ) {
+			switch( $hook->type ){
+				case 'staticPublicFunctionCall':
+					$this->client->out( vsprintf( '- %s > %s >> [%s] %s', array(
+						$hook->resource,
+						$hook->event,
+						$hook->moduleId,
+						$hook->function,
+					) ) );
+					break;
+				case 'inlineFunction':
+					$this->client->out( vsprintf( '- %s > %s >> [%s] %s', array(
+						$hook->resource,
+						$hook->event,
+						$hook->moduleId,
+						'<inline_function> !DEPRECATED!',
+					) ) );
+					break;
+			}
+		}
+	}
+}
