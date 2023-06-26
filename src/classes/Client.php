02239f72522623e42327ab47527ced8defe61734
Christian Würker <christian.wuerker@check24.de>
2023-06-26 09:27:59 +0200

    Set version to 1.0.0a.

--- src/classes/Client.php
+++ src/classes/Client.php
@@ -60,7 +60,7 @@ class Hymn_Client
 
 	public static string $language				= 'en';
 
-	public static string $version				= '0.9.11a';
+	public static string $version				= '1.0.0a';
 
 	public static string $mode					= 'prod';
 
